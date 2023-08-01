573209d791c8a5df5db9a2e59a2fa728d0f42588
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-01 14:04:00 +0600

    Export Package

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export {getSpeakOptionsInsertSerialOperations, type InsertSpeakOptionsParam} from './get-speak-options-serial-operations'
 export {PACKAGE_NAME} from './package-name'
 export {LinkName} from './link-name'
+export {Package,PackageOptions} from './package'
