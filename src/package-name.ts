e8f11b6714eb7c0e0c3f17d93b8f39d7e0f6158d
FreePhoenix888 <freephoenix888@gmail.com>
2023-09-16 23:23:08 +0600

    Add functionality draft

--- src/package-name.ts
+++ src/package-name.ts
@@ -1,4 +1,1 @@
-/**
- * Name of the package
- */
-export const PACKAGE_NAME = "@deep-foundation/capacitor-screen-reader";
+export const CAPACITOR_SCREEN_READER_PACKAGE_NAME = "@deep-foundation/capacitor-screen-reader";
